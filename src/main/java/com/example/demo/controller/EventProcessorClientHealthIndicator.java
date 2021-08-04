d6c51cf9a9e7a5330e8077fddaf9d152f0dd21fc
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2021-08-04 14:02:00 +0000

    Using last event processed's enqueued time to check liveness with a partition.

--- src/main/java/com/example/demo/controller/EventProcessorClientHealthIndicator.java
+++ src/main/java/com/example/demo/controller/EventProcessorClientHealthIndicator.java
@@ -1,19 +1,32 @@
 package com.example.demo.controller;
 
 import com.azure.messaging.eventhubs.EventProcessorClient;
+import com.example.demo.service.LastEventProcessedTrackingService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
 
 @Component
 @AllArgsConstructor
 public class EventProcessorClientHealthIndicator implements HealthIndicator {
 
     private final EventProcessorClient eventProcessorClient;
+    private final LastEventProcessedTrackingService lastEventProcessedTrackingService;
 
     @Override
     public Health health() {
+
+        final long currentTimestamp = Instant.now().toEpochMilli();
+
+        if (currentTimestamp - lastEventProcessedTrackingService.getOldestEventProcessedInAnyPartitionTimestamp() > 300_000) {
+            return Health.down()
+                    .withDetail("Events not consumed one partition since last 5 mins", "")
+                    .build();
+        }
+
         if (eventProcessorClient.isRunning()) {
             return Health.up().build();
         }
